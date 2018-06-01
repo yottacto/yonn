a5299059f79cb531781c0d3713426827d7d0713a
yottacto <yotta.w.cto@gmail.com>
2018-06-02 00:12:25 +0800

    @ add convolutional layer to yonn.hh

--- src/yonn.hh
+++ src/yonn.hh
@@ -2,9 +2,13 @@
 #include "network.hh"
 #include "tensor.hh"
 #include "util.hh"
+
 #include "loss-function/loss-function.hh"
+
 // TODO header file include all layers
 #include "layer/fully-connected-layer.hh"
+#include "layer/convolutional-layer.hh"
+
 #include "optimizer/optimizer.hh"
 
 #include "io/mnist-parser.hh"
