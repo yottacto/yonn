a732f67bb29c5ba9546079006f3b9c29a77e1617
yottacto <yotta.w.cto@gmail.com>
2018-06-10 17:30:21 +0800

    @ making leaky_relu layer to support opencl backend

--- src/activation/leaky_relu.hh
+++ src/activation/leaky_relu.hh
@@ -1,5 +1,8 @@
 #pragma once
+#include <iostream>
+
 #include <cmath>
+#include <CL/cl.hpp>
 #include "layer/layer.hh"
 #include "util/util.hh"
 #include "tensor.hh"
@@ -13,6 +16,15 @@ namespace activation
 
 struct leaky_relu : layer
 {
+    using fk_type = cl::make_kernel<
+        value_type,
+        cl::Buffer&, cl::Buffer&
+    >;
+
+    using bk_type = cl::make_kernel<
+        cl::Buffer&, cl::Buffer&, cl::Buffer&
+    >;
+
     explicit leaky_relu(value_type epsilon = 0.01)
         : layer({data_type::data}, {data_type::data}), epsilon{epsilon}
     {}
@@ -47,7 +59,23 @@ struct leaky_relu : layer
     void init_engine(
         core::backend_type const& backend,
         core::engine::engine_type& eng
-    ) override;
+    ) override
+    {
+        ignore(eng);
+
+        // this backend cannot be network_default
+        if (this->backend == core::backend_type::network_default)
+            layer::set_engine(backend);
+        if (backend == core::backend_type::opencl) {
+            auto& e = std::get<core::engine::opencl>(eng);
+            init_opencl_kernel(e);
+        }
+    }
+
+    void init_opencl_kernel(core::engine::opencl& eng);
+    void init_opencl(core::engine::opencl& eng, size_t size);
+
+    void allocate_nsamples_opencl(size_t batch_size, core::engine::opencl& e) override;
 
     void forward_propagation(core::engine::engine_type& eng, bool united_backend) override;
     void backward_propagation(core::engine::engine_type& eng, bool united_backend) override;
@@ -57,76 +85,123 @@ struct leaky_relu : layer
 
 
     value_type epsilon;
+
+    bool opencl_kernel_initialized{false};
+    std::unique_ptr<fk_type> fk;
+    std::unique_ptr<bk_type> bk;
+    std::unique_ptr<cl::EnqueueArgs> fk_eargs;
+    std::unique_ptr<cl::EnqueueArgs> bk_eargs;
+    cl::Program::Sources sources;
+    cl::Program program;
 };
 
-
-// FIXME this is copy from conv
-void leaky_relu::init_engine(
-    core::backend_type const& backend,
-    core::engine::engine_type& eng
-)
+// implementation of leaky_relu
+void leaky_relu::allocate_nsamples_opencl(size_t batch_size, core::engine::opencl& e)
 {
-    // this backend cannot be network_default
-    if (this->backend == core::backend_type::network_default)
-        layer::set_engine(backend);
-
-    // internal is inited in ctor
+    this->batch_size = batch_size;
     if (backend == core::backend_type::opencl) {
-        auto const& e = std::get<core::engine::opencl>(eng);
-        input[0] = std::make_shared<edge>();
-        input[1] = std::make_shared<edge>(input_shape(1), e.context);
-        input[2] = std::make_shared<edge>(input_shape(2), e.context);
+        input[0]->allocate_nsamples(batch_size, input_shape(0), e.context);
+        output[0]->allocate_nsamples(batch_size, output_shape(0), e.context);
 
-        output[0] = std::make_shared<edge>();
+        init_opencl(e, batch_size * input_shape(0).size());
+    } else {
+        // TODO error or currently not supportted backend
     }
 }
 
+void leaky_relu::init_opencl_kernel(core::engine::opencl& eng)
+{
+    if (!opencl_kernel_initialized) {
+        sources.emplace_back(opencl_kernel::leaky_relu_kernel_code.c_str(), opencl_kernel::leaky_relu_kernel_code.size());
+        program = cl::Program{eng.context, sources};
+        if (program.build({eng.default_device}) != CL_SUCCESS) {
+            // FIXME
+            std::cerr << "Error building: "
+                << program.getBuildInfo<CL_PROGRAM_BUILD_LOG>(eng.default_device) << "\n";
+            throw;
+        }
+
+        opencl_kernel_initialized = true;
+    }
+    fk = std::make_unique<fk_type>(program, "forward");
+    bk = std::make_unique<bk_type>(program, "backward");
+}
+
+void leaky_relu::init_opencl(core::engine::opencl& eng, size_t size)
+{
+    fk_eargs = std::make_unique<cl::EnqueueArgs>(eng.queue, cl::NDRange(size));
+    bk_eargs = std::make_unique<cl::EnqueueArgs>(eng.queue, cl::NDRange(size));
+}
 
 void leaky_relu::forward_propagation(core::engine::engine_type& eng, bool united_backend)
 {
-    ignore(eng);
-    ignore(united_backend);
+    auto const& backend = layer::engine();
+    if (backend == core::backend_type::internal) {
+        tensor const& in_data  = *(input[0] ->get_data());
+        tensor&       out_data = *(output[0]->get_data());
 
-    // TODO init once
-    // TODO const in data?
-    tensor const& in_data  = *(input[0] ->get_data());
-    tensor&       out_data = *(output[0]->get_data());
+        for (size_t sample{0}; sample < in_data.size(); sample++)
+            forward_activation(in_data[sample], out_data[sample]);
 
-    for (size_t sample{0}; sample < in_data.size(); sample++)
-        forward_activation(in_data[sample], out_data[sample]);
+    } else if (backend == core::backend_type::opencl) {
+        if (!united_backend) {
+            auto& e = std::get<core::engine::opencl>(eng);
+            input[0]->set_data(tensor_to_vector(input[0]->data), e);
+        }
+
+        cl::Buffer& in_data  = *(input[0] ->get_data_buffer());
+        cl::Buffer& out_data = *(output[0]->get_data_buffer());
+        (*fk)(*fk_eargs, epsilon, in_data, out_data);
+
+        if (!united_backend) {
+            auto& e = std::get<core::engine::opencl>(eng);
+            vector_to_tensor(output[0]->get_data(e), output[0]->data);
+        }
+    }
 }
 
 void leaky_relu::backward_propagation(core::engine::engine_type& eng, bool united_backend)
 {
-    ignore(eng);
-    ignore(united_backend);
+    auto const& backend = layer::engine();
+    if (backend == core::backend_type::internal) {
+        tensor const& in_data  = *(input[0] ->get_data());
+        tensor&       in_grad  = *(input[0] ->get_grad());
+        tensor const& out_data = *(output[0]->get_data());
+        tensor const& out_grad = *(output[0]->get_grad());
 
-    tensor const& in_data  = *(input[0] ->get_data());
-    tensor&       in_grad  = *(input[0] ->get_grad());
-    tensor const& out_data = *(output[0]->get_data());
-    tensor const& out_grad = *(output[0]->get_grad());
+        for (size_t sample{0}; sample < in_data.size(); sample++)
+            backward_activation(
+                in_data[sample],  in_grad[sample],
+                out_data[sample], out_grad[sample]
+            );
+    } else if (backend == core::backend_type::opencl) {
+        if (!united_backend) {
+            auto& e = std::get<core::engine::opencl>(eng);
+            output[0]->set_data(tensor_to_vector(output[0]->data), e);
+            output[0]->set_grad(tensor_to_vector(output[0]->grad), e);
+        }
 
-    for (size_t sample{0}; sample < in_data.size(); sample++)
-        backward_activation(
-            in_data[sample],  in_grad[sample],
-            out_data[sample], out_grad[sample]
-        );
+        cl::Buffer& in_grad  = *(input[0] ->get_grad_buffer());
+        cl::Buffer& out_data = *(output[0]->get_data_buffer());
+        cl::Buffer& out_grad = *(output[0]->get_grad_buffer());
+        (*bk)(*bk_eargs, out_data, out_grad, in_grad);
+
+        if (!united_backend) {
+            auto& e = std::get<core::engine::opencl>(eng);
+            // vector_to_tensor(input[i]->get_data(e), input[i]->data);
+            vector_to_tensor(input[0]->get_grad(e), input[0]->grad);
+        }
+    }
 }
 
 void leaky_relu::forward_activation(vec_t const& in, vec_t& out)
 {
-    #if USE_OPENMP
-    #pragma omp for
-    #endif
     for (size_t i = 0; i < in.size(); i++)
         out[i] = in[i] > value_type{0} ? in[i] : epsilon * in[i];
 }
 
 void leaky_relu::backward_activation(vec_t const& x, vec_t& dx, vec_t const& y, vec_t const& dy)
 {
-    #if USE_OPENMP
-    #pragma omp for
-    #endif
     for (size_t i = 0; i < x.size(); i++)
         dx[i] = dy[i] * (y[i] > value_type{0} ? value_type(1) : epsilon);
 }
