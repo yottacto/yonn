a4434dd0d2a8ea7e12d1aebba23767b98e17d232
yottacto <yotta.w.cto@gmail.com>
2018-06-11 00:50:23 +0800

    + tested fully

---
+++ tests/backend/fully/main.cc
@@ -0,0 +1,68 @@
+#include <iostream>
+#include <iomanip>
+#include <random>
+#include <vector>
+#include "yonn.hh"
+
+void random_generate(yonn::tensor& t)
+{
+    std::random_device rd;
+    std::mt19937 gen(rd());
+    std::uniform_real_distribution<> dis(-1, 1);
+    for (auto& v : t)
+        for (auto& i : v)
+            i = dis(gen);
+}
+
+void print(yonn::tensor const& t)
+{
+    for (auto v : t) {
+        for (auto i : v)
+            std::cout << std::fixed << std::setprecision(12) << i << " ";
+        std::cout << "\n";
+    }
+    std::cout << "\n";
+}
+
+int main()
+{
+    std::vector<yonn::tensor> in{
+        {{0, 0, 0, 0}, {0, 0, 0, 0}},
+
+        {{0, 0, 0, 0, 0, 0, 0, 0}},
+
+        {{0, 0}},
+    };
+    yonn::tensor dout{
+        {0, 0},
+        {0, 0}
+    };
+
+    for (auto& i : in)
+        random_generate(i);
+    random_generate(dout);
+
+
+    using fc = yonn::fully_connected_layer;
+    auto opencl   = yonn::core::backend_type::opencl;
+    auto internal = yonn::core::backend_type::internal;
+
+    yonn::network<yonn::topo::sequential> net;
+    auto& eng = net.net.eng;
+    auto& e   = std::get<yonn::core::engine::opencl>(eng);
+
+    auto back = internal;
+
+    auto l = fc(4, 2, back);
+    l.init_engine(back, eng);
+    if (back == internal)
+        l.allocate_nsamples(2);
+    else if (back == opencl)
+        l.allocate_nsamples_opencl(2, e);
+
+    // print(yonn::gradient_check(l, in, dout, eng)[0]);
+    std::cout << yonn::gradient_check(l, in, dout, eng) << "\n";
+
+    std::cout << "hello fully\n";
+}
+
