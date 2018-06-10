43f44371b6f215437810fbc03b6db5480e05994f
yottacto <yotta.w.cto@gmail.com>
2018-06-10 15:45:05 +0800

    @ making average_pooling_layer support opencl backend

--- src/layer/average-pooling-layer.hh
+++ src/layer/average-pooling-layer.hh
@@ -14,7 +14,7 @@ namespace yonn
 
 struct average_pooling_layer : layer
 {
-    // TODO backend
+    // FIXME default has_bias
     average_pooling_layer(
         size_t width,
         size_t height,
@@ -26,7 +26,7 @@ struct average_pooling_layer : layer
         core::backend_type backend = core::layer_default_engine()
     ) :
         // FIXME layer need backend
-        layer(std_input_types(has_bias), {data_type::data}),
+        layer(std_input_types(has_bias), {data_type::data}, backend),
         params(
             shape3d_t{width, height, in_channels},
             pool_width,
@@ -44,19 +44,20 @@ struct average_pooling_layer : layer
 
         out_shapes.emplace_back(params.out.size(), 1, 1);
 
+        // TODO deprecated
         // invariant, all input channels allocated in constructor
         // TODO reasoning about this input_shape
-        input[0] = std::make_shared<edge>(input_shape(0));
-        input[1] = std::make_shared<edge>(input_shape(1));
-        input[2] = std::make_shared<edge>(input_shape(2));
+        // input[0] = std::make_shared<edge>(input_shape(0));
+        // input[1] = std::make_shared<edge>(input_shape(1));
+        // input[2] = std::make_shared<edge>(input_shape(2));
 
-        output[0] = std::make_shared<edge>(output_shape(0));
+        // output[0] = std::make_shared<edge>(output_shape(0));
 
-        // TODO init different kernel
+        // // TODO init different kernel
 
-        for (size_t i{0}; i < in_types.size(); i++)
-            if (in_types[i] == data_type::weight)
-                init_weight(input[i]->data[0], fan_in_size(), fan_out_size());
+        // for (size_t i{0}; i < in_types.size(); i++)
+        //     if (in_types[i] == data_type::weight)
+        //         init_weight(input[i]->data[0], fan_in_size(), fan_out_size());
     }
 
     average_pooling_layer(
@@ -128,6 +129,8 @@ struct average_pooling_layer : layer
         core::engine::engine_type& eng
     ) override;
 
+    void allocate_nsamples_opencl(size_t batch_size, core::engine::opencl& e) override;
+
     void forward_propagation(core::engine::engine_type& eng, bool united_backend) override;
     void backward_propagation(core::engine::engine_type& eng, bool united_backend) override;
 
@@ -136,8 +139,8 @@ struct average_pooling_layer : layer
     core::avg_pool_parameter params;
     core::framework::op_kernel_context forward_context;
     core::framework::op_kernel_context backward_context;
-    std::shared_ptr<core::framework::op_kernel> forward_kernel;
-    std::shared_ptr<core::framework::op_kernel> backward_kernel;
+    std::shared_ptr<core::kernel::average_pooling_op> forward_kernel;
+    std::shared_ptr<core::kernel::average_pooling_grad_op> backward_kernel;
 };
 
 void average_pooling_layer::init_engine(
@@ -149,14 +152,53 @@ void average_pooling_layer::init_engine(
     if (this->backend == core::backend_type::network_default)
         layer::set_engine(backend);
 
-    // internal is inited in ctor
-    if (backend == core::backend_type::opencl) {
-        auto const& e = std::get<core::engine::opencl>(eng);
+    if (backend == core::backend_type::internal) {
+        // invariant, all input channels allocated in constructor
+        // TODO reasoning about this input_shape
+        input[0] = std::make_shared<edge>();
+        input[1] = std::make_shared<edge>(input_shape(1));
+        input[2] = std::make_shared<edge>(input_shape(2));
+
+        output[0] = std::make_shared<edge>();
+
+        for (size_t i{0}; i < in_types.size(); i++)
+            if (in_types[i] == data_type::weight)
+                init_weight(input[i]->data[0], fan_in_size(), fan_out_size());
+    } else if (backend == core::backend_type::opencl) {
+        auto& e = std::get<core::engine::opencl>(eng);
         input[0] = std::make_shared<edge>();
         input[1] = std::make_shared<edge>(input_shape(1), e.context);
         input[2] = std::make_shared<edge>(input_shape(2), e.context);
 
         output[0] = std::make_shared<edge>();
+
+        for (size_t i{0}; i < in_types.size(); i++)
+            if (in_types[i] == data_type::weight)
+                init_weight(input[i]->data[0], fan_in_size(), fan_out_size());
+
+        for (auto i = 1u; i < input.size(); i++)
+            this->input[i]->set_data(tensor_to_vector(input[i]->data), e);
+
+        forward_kernel->init_opencl_kernel(e);
+        backward_kernel->init_opencl_kernel(e);
+    }
+}
+
+void average_pooling_layer::allocate_nsamples_opencl(size_t batch_size, core::engine::opencl& e)
+{
+    this->batch_size = batch_size;
+    if (backend == core::backend_type::opencl) {
+        input[0]->allocate_nsamples(batch_size, input_shape(0), e.context);
+        output[0]->allocate_nsamples(batch_size, output_shape(0), e.context);
+
+        forward_kernel->init_opencl(e, batch_size * params.out.size(), batch_size);
+        backward_kernel->init_opencl(e, {
+            batch_size * params.in.size(),
+            params.out.depth,
+            params.out.depth,
+        }, batch_size);
+    } else {
+        // TODO error or currently not supportted backend
     }
 }
 
@@ -176,6 +218,12 @@ void average_pooling_layer::forward_propagation(core::engine::engine_type& eng, 
             out_data[i].emplace<tensor*>(output[i]->get_data());
 
     } else if (backend == core::backend_type::opencl) {
+        if (!united_backend) {
+            auto& e = std::get<core::engine::opencl>(eng);
+            for (size_t i{0}; i < in_channels; i++)
+                input[i]->set_data(tensor_to_vector(input[i]->data), e);
+        }
+
         for (size_t i{0}; i < in_channels; i++)
             in_data[i].emplace<cl::Buffer*>(input[i]->get_data_buffer());
 
@@ -187,6 +235,14 @@ void average_pooling_layer::forward_propagation(core::engine::engine_type& eng, 
     forward_context.set_engine(layer::engine());
 
     forward_kernel->compute(forward_context, eng, united_backend);
+
+    if (backend == core::backend_type::opencl) {
+        if (!united_backend) {
+            auto& e = std::get<core::engine::opencl>(eng);
+            for (size_t i{0}; i < out_channels; i++)
+                vector_to_tensor(output[i]->get_data(e), output[i]->data);
+        }
+    }
 }
 
 void average_pooling_layer::backward_propagation(core::engine::engine_type& eng, bool united_backend)
@@ -211,6 +267,14 @@ void average_pooling_layer::backward_propagation(core::engine::engine_type& eng,
         }
 
     } else if (backend == core::backend_type::opencl) {
+        if (!united_backend) {
+            auto& e = std::get<core::engine::opencl>(eng);
+            for (size_t i{0}; i < out_channels; i++) {
+                output[i]->set_data(tensor_to_vector(output[i]->data), e);
+                output[i]->set_grad(tensor_to_vector(output[i]->grad), e);
+            }
+        }
+
         for (size_t i{0}; i < in_channels; i++) {
             in_data[i].emplace<cl::Buffer*>(input[i]->get_data_buffer());
             in_grad[i].emplace<cl::Buffer*>(input[i]->get_grad_buffer());
@@ -226,6 +290,16 @@ void average_pooling_layer::backward_propagation(core::engine::engine_type& eng,
     backward_context.set_engine(layer::engine());
 
     backward_kernel->compute(backward_context, eng, united_backend);
+
+    if (backend == core::backend_type::opencl) {
+        if (!united_backend) {
+            auto& e = std::get<core::engine::opencl>(eng);
+            for (size_t i{0}; i < in_channels; i++) {
+                vector_to_tensor(input[i]->get_data(e), input[i]->data);
+                vector_to_tensor(input[i]->get_grad(e), input[i]->grad);
+            }
+        }
+    }
 }
 
 } // namespace yonn
